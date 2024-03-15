8f1e3c360ac68ed7de1514b1937d53ce7eef6d01
niialabi <taufiq.alabi@gmail.com>
2024-03-15 05:24:09 +0000

    type anno sum lists

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Sum list function module """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ returns sum of list """
+    return sum(input_list)
