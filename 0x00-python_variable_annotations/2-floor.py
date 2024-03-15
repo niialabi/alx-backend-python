6dc3e9c77d9976664a06682b22210c3fa55f03cb
niialabi <taufiq.alabi@gmail.com>
2024-03-15 05:10:23 +0000

    type annon floor func

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" floor function module """
+
+from math import floor as floor
+
+
+def floor(n: float) -> int:
+    """
+    func which takes floor as argument and returns floor of n
+    """
+    return floor(n)
