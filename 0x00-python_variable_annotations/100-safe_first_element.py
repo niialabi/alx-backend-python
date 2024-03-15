4ca4b705a40378de62ea8e1e26d3e9f20854ff70
niialabi <taufiq.alabi@gmail.com>
2024-03-15 05:44:03 +0000

    type annon: first element type

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+!/usr/bin/env python3
+""" safe_first_element func module """
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ takes first element of seq and returns any/none """
+    if lst:
+        return lst[0]
+    else:
+        return None
