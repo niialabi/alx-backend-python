886450a921d3ecaa43bc31226d65f18639f53a2b
niialabi <taufiq.alabi@gmail.com>
2024-03-18 19:00:43 +0000

    function signature fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ wait_random module """
-
 import random
 import asyncio
 
 
-async def wait_random(max_dela: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ ret random number btw 0 & max_dela """
     wait_time = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
