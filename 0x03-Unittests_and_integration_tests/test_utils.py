a0f435ff76e512bf659615bd0d913884e60830e2
niialabi <taufiq.alabi@gmail.com>
2024-04-08 16:17:16 +0000

    unittests for get_json method added

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,10 @@
 
 import unittest
 from typing import Any, Mapping, Sequence, Dict
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest.mock import patch, Mock
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -36,3 +38,20 @@ class TestAccessNestedMap(unittest.TestCase):
         """ test exceptions for access nested map fun """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ unittest test get json  method """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict):
+        """ test method if expected results is returned """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        with patch('requests.get', return_value=mock_response) as mock_get:
+            output = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(output, test_payload)
